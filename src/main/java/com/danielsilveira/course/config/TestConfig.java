0e4bd676bbcc46ee4abcd417b6085e079d7428b3
Daniel Silveira <danielsilveiraluna07@gmail.com>
2021-06-14 09:11:22 -0300

    Category

--- src/main/java/com/danielsilveira/course/config/TestConfig.java
+++ src/main/java/com/danielsilveira/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.danielsilveira.course.entities.Category;
 import com.danielsilveira.course.entities.Order;
 import com.danielsilveira.course.entities.User;
 import com.danielsilveira.course.entities.enums.OrderStatus;
+import com.danielsilveira.course.repositories.CategoryRepository;
 import com.danielsilveira.course.repositories.OrderRepository;
 import com.danielsilveira.course.repositories.UserRepository;
 
@@ -25,9 +27,18 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
@@ -37,6 +48,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
 	}
 	
 	
