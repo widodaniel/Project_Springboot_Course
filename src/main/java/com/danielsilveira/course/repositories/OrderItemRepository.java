c4ebe8aaf3d7bbc96388303c6fa8efdaac2ff30d
Daniel Silveira <danielsilveiraluna07@gmail.com>
2021-06-15 08:31:02 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

---
+++ src/main/java/com/danielsilveira/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.danielsilveira.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.danielsilveira.course.entities.OrderItem;
+
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+}
